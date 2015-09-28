570c4d7fa822784439c266ecd9dad5a123b31b34
diego andres sanchez molina <diegoas@zeta40.aulas.gsyc.es>
2015-09-28 10:52:02 +0200

    calculadora

--- calc.py
+++ calc.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-# holaaaaaaaaaaaaaaa
+
 # Vamos a programar una calculadora
 
 def main():
     pass
 
+def suma(operando1,operando2):
+	return operando1 + operando2
+	
+def resta(operando1,operando2):
+	return mayor - menor
 if __name__ == "__main__":
     main()
